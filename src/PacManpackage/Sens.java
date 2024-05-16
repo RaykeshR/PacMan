1121f4ab289b51c67213dec1b5debab7a6fdbd3f
Raykesh <raykesh.ravirooban@reseau.eseo.>
2024-05-16 09:27:27 +0200

    Changes to be committed: 	modified:   bin/PacManpackage/PacMan.class 	new file:   src/PacManpackage/Sens.java

---
+++ src/PacManpackage/Sens.java
@@ -0,0 +1,56 @@
+package PacManpackage;
+
+public enum Sens {
+	    NONE(0, 0),
+	    UP(-1, 0),
+	    DOWN(1, 0),
+	    LEFT(0, -1),
+	    RIGHT(0, 1);
+
+	    private final int dx;
+	    private final int dy;
+
+	    Sens(int dx, int dy) {
+	        this.dx = dx;
+	        this.dy = dy;
+	    }
+
+	    public Sens turnLeft() {
+	        switch (this) {
+	            case UP:
+	                return LEFT;
+	            case DOWN:
+	                return RIGHT;
+	            case LEFT:
+	                return DOWN;
+	            case RIGHT:
+	                return UP;
+	            default:
+	                return NONE;
+	        }
+	    }
+
+	    public Sens turnRight() {
+	        switch (this) {
+	            case UP:
+	                return RIGHT;
+	            case DOWN:
+	                return LEFT;
+	            case LEFT:
+	                return UP;
+	            case RIGHT:
+	                return DOWN;
+	            default:
+	                return NONE;
+	        }
+	    }
+
+	    public int getDx() {
+	        return dx;
+	    }
+
+	    public int getDy() {
+	        return dy;
+	    }
+
+}
