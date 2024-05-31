45aa5632830ed199fc606026f4336367eea06e29
Raykesh <raykesh.ravirooban@reseau.eseo.>
2024-05-31 11:30:49 +0200

    Changes to be committed: 	modified:   bin/PacManpackage/Fantome.class 	modified:   bin/PacManpackage/Plateau.class 	modified:   src/PacManpackage/Fantome.java 	modified:   src/PacManpackage/Plateau.java

--- src/PacManpackage/Fantome.java
+++ src/PacManpackage/Fantome.java
@@ -37,7 +37,8 @@ public class Fantome {
 	 * @Fonctionnement : Verifie la présence de mur dans le Plateau 
 	 * @return Une matrice de 3 par 3 indiquant la position des mur alentoure du labyrinte
 	 * 1 indique la présence d'un mur et 0 l'absence d'un mur . 
-	 */
+	 * Finalement c'est dans Plateau
+	 */ 
 	int[][] mur() {
 		int[][] L = new int[][] {
 			{0,0,0},
@@ -53,16 +54,69 @@ public class Fantome {
 	 * @Fonctionnement : Définie le mouvement aléatoire des fantômes 
 	 */
 	void MouvementAuto() {
-		//TODO
-		if(mur()[1+S.getDx()][1+S.getDy()]==0) {
+		int Decision = getRandomNumber(1,100); // [ 1 , 100 ] 
+		int[][] M=mur();
+		if( M[1+S.getDx()][1+S.getDy()]==0 && Decision >= 95 ) {
 			//Si il n'y a pas de mur dans le sens courant on continue (95% du temps)
 			int n =1; // déplacement d'une Case
 			Goto(x+S.getDx()*n,y+S.getDy()*n);
+		}else {
+			// Choisi une direction parmi celle disponible.
+			int SensPossible = (1-M[0][1]) + (1-M[1][0]) + (1-M[2][1]) + (1-M[1][2]);
+			Sens[] NouveauSens = new Sens[SensPossible];
+			if (SensPossible!=0) {
+				int DecisionNouveauSens = getRandomNumber(0,SensPossible-1); // [ 0 , SensPossible [ 
+				// Remplir NouveauSens
+				int index=0;
+				if (M[0][1]==0) {
+					// Case du haut disponible
+					NouveauSens[index] = Sens.UP;
+					index++;
+				}
+				if (M[1][0]==0) {
+			        // Case de la gauche disponible
+			        NouveauSens[index] = Sens.LEFT;
+			        index++;
+			    }
+			    if (M[2][1]==0) {
+			        // Case du bas disponible
+			        NouveauSens[index] = Sens.DOWN;
+			        index++;
+			    }
+			    if (M[1][2]==0) {
+			        // Case de la droite disponible
+			        NouveauSens[index] = Sens.RIGHT;
+			        index++;
+			    }
+			    // Nouvelle direction choisi
+			    S = NouveauSens[DecisionNouveauSens];
+			    
+			    MouvementAuto(); 
+			    // Case disponible donc mouvement
+			    // Boucle infinie en cas de bug ou d'erreur ! ! !
+			    
+			}else{
+				// Bloquer pas de sens possible
+			}
+			
 		}
 	}
 	
-	
-	
+	/**
+	 * @Fonctionnement : Définie un randint aléatoire entre  [min,max] <===
+	 * @param min
+	 * @param max
+	 * @return random int between [ min , max ]
+	 */
+	public int getRandomNumber(int min, int max) {
+	    return (int) ((Math.random() * (max+1 - min)) + min);
+	    /*
+	     * random  											[0,1[
+	     * random * (max+1 - min)  							[0,max+1-min[
+	     * ((Math.random() * (max - min)) + min)  			[min,max+1[
+	     * (int) ((Math.random() * (max+1 - min)) + min)  	[min,max]
+	     */
+	}
 	
 	
 	
--- src/PacManpackage/Plateau.java
+++ src/PacManpackage/Plateau.java
@@ -69,21 +69,21 @@ public class Plateau {
 	/**
 	 * @return the p_original
 	 */
-	public int[][] getP_original() {
+	public int[][] getPlateau() {
 		return P_original;
 	}
 
 	/**
 	 * @param p_original the p_original to set
 	 */
-	public void setP_original(int[][] p_original) {
+	public void setPlateau(int[][] p_original) {
 		P_original = p_original;
 	}
 
 	/**
 	 * @param p_original the p_original to set
 	 */
-	public void setP(int x,int y,int valeur) {
+	public void setPlateau(int x,int y,int valeur) {
 		P_original[x][y] = valeur;
 	}
 	
