1bb90d7bfb31b2cc0a11cdf6ecd846a204f95285
Raykesh <raykesh.ravirooban@reseau.eseo.>
2024-05-31 09:08:41 +0200

    Changes to be committed: 	modified:   bin/PacManpackage/Fantome.class 	modified:   bin/PacManpackage/PacMan.class 	modified:   src/PacManpackage/Fantome.java

--- src/PacManpackage/Fantome.java
+++ src/PacManpackage/Fantome.java
@@ -5,8 +5,19 @@ import javax.swing.JLabel;
 public class Fantome {
 
 	JLabel label;
+	Sens S;
+	//position : en coordonné 
+	double x,y;
+	// Position dans le Plateau (Matrice)
+	double Px,Py;
 	public Fantome(JLabel LeLabelDuFantome) {
 		this.label = LeLabelDuFantome;
+	}
+	
+	public Fantome(JLabel LeLabelDuFantome ,double x,double y,double Px,double Py) {
+		this.label = LeLabelDuFantome;
+		this.x=x;this.y=y;
+		this.Px=Px;this.Py=Py;
 	}
 	
 	void Goto(double x,double y) {
