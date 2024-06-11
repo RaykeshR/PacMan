771d26ea18faa3d6390d7d95d8ba9c5cad7623f4
mcoumba <160884847+mcoumba@users.noreply.github.com>
2024-06-11 12:47:16 +0200

    Update Point.java

--- src/PacManpackage/Point.java
+++ src/PacManpackage/Point.java
@@ -1,3 +1,4 @@
+//librairie
 package PacManpackage;
 
 import javax.swing.JLabel;
@@ -5,27 +6,29 @@ import javax.swing.JLayeredPane;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
+
 public class Point {
-    private JLabel label;
-    int x;
+	private JLabel label;
+	int x;
 	int y;
-    private final int SIZE = 5;
-    private final int offsetX = 110;
-    private final int offsetY = 105;
+	private final int SIZE = 5;
+	private final int offsetX = 110;
+	private final int offsetY = 105;
 //    private boolean EnCoursDeDigestion;
 //    // Position dans le Plateau (Matrice)
 	int Px,Py;
 
-
+//constructeur
     public Point(int x, int y) {
         this.x = x;Px=x;
         this.y = y;Py=y;
-        ImageIcon icon = new ImageIcon(this.getClass().getResource("/PacManpackage/rond-jaune.png"));
-        icon = resizeImageIcon(icon, 3, 3);
-        label = new JLabel(icon);
-        label.setBounds(x * SIZE, y * SIZE, icon.getIconWidth(), icon.getIconHeight());
+        ImageIcon icon = new ImageIcon(this.getClass().getResource("/PacManpackage/rond-jaune.png"));// pour recuperer l'information (l'image rond-jaune)
+        icon = resizeImageIcon(icon, 3, 3); // pour redimensionner l'image
+        label = new JLabel(icon); //nouveau label associé à icon
+        label.setBounds(x * SIZE, y * SIZE, icon.getIconWidth(), icon.getIconHeight());//Définit la position et la taille du label pour afficher le point
     }
-
+//methodes
+	//methodes pour redimensionner la taille d'un icon
     private ImageIcon resizeImageIcon(ImageIcon icon, int width, int height) {
         Image img = icon.getImage();
         Image resizedImage = img.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
@@ -36,16 +39,7 @@ public class Point {
         return label;
     }
 
-    public void addToPane(JLayeredPane pane) {
-        pane.add(label, JLayeredPane.DEFAULT_LAYER);
-        pane.repaint();
-    }
-
-    public void removeFromPane(JLayeredPane pane) {
-        pane.remove(label);
-        pane.repaint();
-    }
-
+   
     public int getX() {
         return x;
     }
@@ -53,6 +47,7 @@ public class Point {
     public int getY() {
         return y;
     }
+    
 
 
 //    public Point(int x, int y) {
@@ -114,12 +109,12 @@ public class Point {
 //	    }
 //	    return points;
 //	}
-
-	private int count(int[][] matrix, int value) {
+//methode pour compter dans la matrice le nombre de fois qu'il y'a valeur
+	private int count(int[][] matrix, int valeur) {
 	    int count = 0;
 	    for (int i = 0; i < matrix.length; i++) {
 	        for (int j = 0; j < matrix[i].length; j++) {
-	            if (matrix[i][j] == value) {
+	            if (matrix[i][j] == valeur) {
 	                count++;
 	            }
 	        }
@@ -128,11 +123,9 @@ public class Point {
 	}
 	
 	
-	/**
-	 * 
-	 * renvoie les objets de la classe point correspondont aux Gros point sur le plateau.
-	 * n=10
-	 */
+	//renvoie les objets de la classe point correspondont aux Gros point sur le plateau.
+	
+	 //
 	public Point[] LesBonus(int n) {
 		Plateau get = new Plateau();
 	    int[][] P = get.getPlateau();
